4c65b5c93774a013fc846d73852ccc6a0b2a1f79
J0sueTM <teodoro.josue@protonmail.ch>
2023-06-15 21:00:21 -0300

    feat: finished new version

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      dark: '#2e3030',
+      light: '#dfd9cf',
+      blue: '#525896'
+    }
+  },
+  plugins: [],
+}
