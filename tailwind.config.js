d7c47148cec0d46d0b4054d2fb871cd16d2c3601
J0sueTM <teodoro.josue@protonmail.ch>
2023-11-25 22:01:21 +0000

    fix: rearrange tailwind cfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./resources/js/app.js'],
+  purge: [],
   theme: {
-    extend: {
-      colors: {
-        'dark0': '#211e20',
-        'dark1': '#555568',
-        'light0': '#e9efec',
-        'light1': '#a0a08b'
-    },
+    colors: {
+      'dark0': '#211e20',
+      'dark1': '#555568',
+      'light0': '#e9efec',
+      'light1': '#a0a08b'
+    }
+    extend: {},
   },
   plugins: [],
 }
